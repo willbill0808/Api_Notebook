04629043725b165b6a1d199acb47a63738ba3ecf
willbill0808 <williamlrosewell08@gmail.com>
2026-03-18 15:20:40 +0100

    layout bug

--- applet.py
+++ applet.py
@@ -1,18 +1,26 @@
 import FreeSimpleGUI as sg
 import requests, json 
 
-tab_layout1 = [[sg.Button('My first Button!'), sg.Checkbox('My first Checkbox!')],
-               [sg.Button('Another Button.')]]
-tab_layout2 = [[sg.Button('My third Button!'), sg.Checkbox('My second Checkbox!')]]
+def tabMaker(Tittle):
+    Form = [sg.Text(Tittle), sg.Multiline(key='-ML-', write_only=True, size=(60,10))]
+    return [Form]
 
-layout = [[sg.TabGroup([[sg.Tab("Tab 1", tab_layout1), sg.Tab("Tab 2", tab_layout2)]])]]
+tab_layout1 = [
+    [sg.Text("Tab Maker")], 
+    [sg.Button("Make new tab")]
+    ]
+
+tabs = [sg.Tab("tabOne", tab_layout1), sg.Tab("tabTwo", tabMaker("tab2"))]
+
+layout = [
+    [sg.Text("Tittle")],
+    #[sg.TabGroup([tabs])],
+    #[sg.Button("quit")]
+    ]
 
 
-layout1 = [
-    [sg.Multiline(key='-ML-', write_only=True, size=(60,10))]
-]
+window = sg.window('Multiline Example', layout)
 
-window = sg.Window('Multiline Example', layout, finalize=True)
 
 while True:
     event, values = window.read()
