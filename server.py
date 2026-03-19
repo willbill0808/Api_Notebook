9b49949879027f0db5a9b133653bc21c5908f880
willbill0808 <williamlrosewell08@gmail.com>
2026-03-19 14:19:11 +0100

    db initial

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
 import sqlite3
 
-conn = sqlite3.connect('example.db')
+server = sqlite3.connect('server.db')
+server.execute("PRAGMA foreign_keys = ON")
 
-conn.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
-conn.execute("INSERT INTO users (name) VALUES (?)", ("Alice",))
-conn.commit()
+server.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(25) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+""")
 
-print(conn.execute("SELECT * FROM users "))
+server.execute("""
+CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    notename VARCHAR(25),
+    contents TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+""")
+
+server.commit()
