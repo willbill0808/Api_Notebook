d6c0972d746729929b41c976df9899a67e0f0ecd
willbill0808 <williamlrosewell08@gmail.com>
2026-03-25 17:40:00 +0100

    feat: server

---
+++ applet4.py
@@ -0,0 +1,47 @@
+import FreeSimpleGUI as sg
+import requests, json 
+user = "test"
+user_id = 1
+
+def tabLoader():
+
+    tabs = [sg.Tab("menu", [[sg.Text('What do you want you new note space to be called:')], [sg.Input(key='-INPUT-'), sg.Button("Make new tab", key=("-Make_tab-"))]])]
+
+    print("hi")
+
+    r = requests.get("http://localhost:8000/notes")  
+    print(r.json())
+
+    rows = r.json()["data"]
+
+    for row in rows:
+        print(row)
+        tabs.append(sg.Tab(row[2], [[sg.Multiline(key=f'-ML-', default_text=row[3], write_only=True, size=(120,20))]]))
+    return tabs
+
+def makeTab(Tittle):
+    print(Tittle)
+
+layout = [
+    [sg.Text("Tittle")],
+    [sg.TabGroup([tabLoader()])],
+    [sg.Button("Quit", key="-Exit-")]
+    ]
+
+window = sg.Window(
+    'Notes App',
+    layout,
+    size=(900, 500),
+    resizable=True,
+    finalize=True
+)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == '-Exit-':
+        break
+    if event =="-Make_tab-":
+        makeTab(values['-INPUT-'])
+
+window.close()
+
---
+++ server2.py
@@ -0,0 +1,39 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sqlite3
+import json
+from urllib.parse import urlparse, parse_qs
+
+# Connect to SQLite database (or creates one)
+conn = sqlite3.connect("server.db", check_same_thread=False)
+cursor = conn.cursor()
+
+# Create a sample table
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed = urlparse(self.path)
+        
+        if parsed.path == "/notes":
+            try:
+                cursor.execute("SELECT * FROM notes")
+                rows = cursor.fetchall()
+
+                response = {
+                    "status": "ok",
+                    "data": rows
+                }
+            except Exception as e:
+                response = {
+                    "status": "error",
+                    "message": str(e)
+                }
+            
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(response).encode())
+
+
+server = HTTPServer(("localhost", 8000), Handler)
+print("Server running at http://localhost:8000")
+server.serve_forever()
